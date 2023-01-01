62c2d7e7e60464d860ec9f31998521d1e0ffd9e5
rick <xyz@abc.com>
2023-01-01 12:22:24 +0530

    In this updated version I added "delete button" to delete stored links 	eventlistener listens for "double click" of button changed some styles of button "save tab" button to save current tabs url also made the listeItemRender fucntion dynamic by adding parrameter "leads" 	change its name to "reder" function The major update is storing list items loccaly with "windows.localstorage" method 	used methods like "steItem" "getItem" "clear" 	for storing purposes used "JSON.parse" & "JSON.stringify" methods

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
 
 //use let only if variable alue is updating in program otherwise use const 
-let myLead = [];
+let myLeads = ["dsf"];
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const delBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {  
-  if (inputEl.value != '') {
-    myLead.push(inputEl.value);
-    listItemRender();
-    inputEl.value = '';
-  }
-})
+//myLeads = JSON.parse(myLeads)
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+if (leadsFromLocalStorage) {
+  myLeads = [...myLeads, ...leadsFromLocalStorage]
+  render(myLeads)
+}
 
-function listItemRender() {
+function render(leads) {
   let ListItem = '';
-  for (let i = 0; i < myLead.length; i++) {
-    ListItem += `<li><a target='_blank' href = '${myLead[i]}'> link${i}</li>`;
+  for (let i = 0; i < leads.length; i++) {
+    ListItem += `<li><a target='_blank' href = '${leads[i]}'> ${leads[i]}</li>`;
   }
   ulEl.innerHTML = ListItem;
 } 
+
+inputBtn.addEventListener("click", () => {  
+  if (inputEl.value != '') {
+    myLeads.push(inputEl.value);
+    render(myLeads);
+    inputEl.value = '';
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  }
+})
+
+delBtn.addEventListener("dblclick", () => {
+  myLeads = []
+  render(myLeads)
+})
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true},function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
+
+function storeLeads() {
+  let myLeadsString = JSON.stringify(myLeads);
+  localStorage.setItem("myLeads", myLeadsString);
+  console.log(localStorage.getItem("myLeads"));
+}
